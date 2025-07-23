9b7f1faa659e3e55e7c215cdfd25fd458a50242e
lolohole <basemhalaika@gmail.com>
2025-07-23 19:41:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -281,10 +281,8 @@ io.on('connection', (socket) => {
 // تشغيل السيرفر
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from app.js!');
+server.listen(port, () => {
+  console.log(`✅ Server running on port ${port}`);
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+
