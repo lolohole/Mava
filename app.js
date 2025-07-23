52123048eebc00556713866ef186bd10f6931f97
lolohole <basemhalaika@gmail.com>
2025-07-23 19:33:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -279,7 +279,7 @@ io.on('connection', (socket) => {
 });
 
 // تشغيل السيرفر
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello from app.js!');
