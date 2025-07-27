5d10dee8f1f7d70c11981a1f64297fb67b2dc847
lolohole <basemhalaika@gmail.com>
2025-07-27 21:12:14 +0300

    Update isAdmin.js

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -1,1 +1,11 @@
 
+// isAdmin.js
+
+const isAdmin = (req, res, next) => {
+  if (req.user && req.user.role === 'admin') {
+    return next();
+  }
+  return res.status(403).send('Unauthorized - Admins only');
+};
+
+module.exports = isAdmin;
